f6ff15d0c2f3aa9e0834ed4cecbbd73c0d4d13d7
Angela Lee <rubii.kun@gmail.com>
2019-03-16 16:53:41 -0700

    Add popular button component

---
+++ react-client/src/components/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Buttons() {
+  return (
+    <div>
+      <button>American</button>
+      <button>Chinese</button>
+      <button>Indian</button>
+      <button>Japanese</button>
+      <button>Vietnamese</button>
+    </div>
+  );
+}
+
+export default Buttons;
