83139e7e2a9625158d10dc14aefb284eba5cac63
Angela Lee <rubii.kun@gmail.com>
2019-03-16 16:54:23 -0700

    Add main heading component

---
+++ react-client/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <h3>What would you like to eat?</h3>
+    </div>
+  );
+}
+
+export default Main;
