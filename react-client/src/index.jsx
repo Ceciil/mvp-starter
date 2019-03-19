cd12deb8c344916a4b82f29648922b666a17f57b
Angela Lee <rubii.kun@gmail.com>
2019-03-18 19:11:33 -0700

    Add additional code

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -3,24 +3,51 @@ import ReactDOM from "react-dom";
 
 import axios from "axios";
 import data from "./components/data.js";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 import Main from "./components/Main.jsx";
 import Buttons from "./components/Buttons.jsx";
 import Search from "./components/Search.jsx";
 import Display from "./components/Display.jsx";
+import Choice from "./components/Choice.jsx";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data.businesses[0],
+      choiceBool: false,
+      choice: data.businesses[1],
+      dataBool: false,
+      data: null,
       search: null
     };
+    this.onSelect = this.onSelect.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onAdd = this.onAdd.bind(this);
   }
 
-  componentDidMount() { }
+  onSelect(event) {
+    console.log(event.target.innerHTML);
+    const category = event.target.innerHTML;
+    axios
+      .get(`/api/category/${category}`)
+      .then(response => {
+        console.log("onSelect: ", response);
+        function getRandomInt(max) {
+          return Math.floor(Math.random() * Math.floor(max));
+        }
+        const randomInt = getRandomInt(response.data.length);
+        console.log(randomInt, response.data[randomInt]);
+        this.setState({
+          choiceBool: true,
+          choice: response.data[randomInt]
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   onChange(event) {
     this.setState({ search: event.target.value });
@@ -32,22 +59,47 @@ class App extends React.Component {
     axios
       .get(`/api/${search}`)
       .then(response => {
-        this.setState({ data: response.data.businesses[0] });
+        this.setState({
+          dataBool: true,
+          data: response.data.businesses
+        });
       })
       .catch(err => {
         console.log(err);
       });
   }
 
+  onAdd(event) {
+    // console.log(item)
+    console.log(event);
+    console.log(event.target);
+  }
+
   render() {
+    let display, choice;
+    if (this.state.choiceBool) {
+      choice = <Choice data={this.state.choice} />;
+    } else {
+      choice = <div />;
+    }
+    if (this.state.dataBool) {
+      display = (
+        <Display data={this.state.data} onAdd={this.onAdd} />
+      );
+    } else {
+      display = <div id="choice" />;
+    }
+
     return (
       <div className="container fade-in">
         <Main />
-        <Buttons />
+        <Link activeClass="active" to="pad" spy={true} smooth={true} duration={500}>
+          <Buttons onClick={this.onSelect} />
+        </Link>
         <Search onChange={this.onChange} onSubmit={this.onSubmit} />
-        <Display data={this.state.data} />
-        <h1>Others</h1>
-        <h1>Footer</h1>
+        {display}
+        {choice}
+        <div id="pad" />
       </div>
     );
   }
