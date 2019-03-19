b6ea3b6d343969f3c7a1bb04590ae23d1cd0613b
Angela Lee <rubii.kun@gmail.com>
2019-03-19 12:01:44 -0700

    Code cleanup

--- react-client/src/components/Choice.jsx
+++ react-client/src/components/Choice.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React from 'react';
 
 function Choice(props) {
   return (
     <div>
-      <h2 className="choice-title">{props.data.name}</h2>
-      <div className="display">
-        <div className="overflow">
-          <img className="fade-in" src={props.data.image_url} height="300px" />
+      <h2 className='choice-title'>{props.data.name}</h2>
+      <div className='display'>
+        <div className='overflow'>
+          <img className='fade-in' src={props.data.image_url} height='300px' />
         </div>
-        <div className="notof">
-          <p>{props.data.name}</p>
+        <div className='notof'>
+          <p className='title'>{props.data.name}</p>
           <p>
             {props.data.location.address1} <br />
             {props.data.location.city} <br />
-            {props.data.location.zip_code}
+            {props.data.location.zip_code} <br />
+            <br />
+            {props.data.display_phone} <br />
+            <br />
+            <a href={props.data.url} target='_blank'>Yelp</a>
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Choice;
