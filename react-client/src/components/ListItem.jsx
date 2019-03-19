24d1d15ac29ecb3e4b7d0988a61dc5609f3cc600
Angela Lee <rubii.kun@gmail.com>
2019-03-18 19:10:56 -0700

    Create individual restaurant listing

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const ListItem = (props) => (
-  <div>
-    { props.item.description }
-  </div>
-)
+function ListItem(props) {
+  return (
+    <div className="display">
+      <div className="overflow">
+        <img className="fade-in" src={props.data.image_url} height="300px" />
+      </div>
+      <div className="overflow">
+        <p>{props.data.name}</p>
+        <p>
+          {props.data.location.address1} <br />
+          {props.data.location.city} <br />
+          {props.data.location.zip_code}
+          <button onClick={props.onAdd}>Add</button>
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default ListItem;
