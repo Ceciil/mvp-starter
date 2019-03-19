6e53d1bd08700af7efb41809e5adcea30cc67d11
Angela Lee <rubii.kun@gmail.com>
2019-03-18 19:10:20 -0700

    Display list of restaurants

--- react-client/src/components/Display.jsx
+++ react-client/src/components/Display.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ListItem from "./ListItem.jsx";
 
 function Display(props) {
+  const listRestaurants = props.data.slice(0, 7).map(item => {
+    return <ListItem data={item} key={item.id} onAdd={props.onAdd} />;
+  });
+
   return (
-    <div className="display">
-      <img className="fade-in" src={props.data.image_url} height="500px" />
+    <div>
+      <h2 className="choice-title">Add your favorite places</h2>
+      <div className="display-div">{listRestaurants}</div>
     </div>
   );
 }
