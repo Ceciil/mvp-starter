7c81bd89eebc3a95e3f268b07c4f99a99b08cc3f
Angela Lee <rubii.kun@gmail.com>
2019-03-19 13:52:52 -0700

    Add additional category buttons

--- react-client/src/components/Buttons.jsx
+++ react-client/src/components/Buttons.jsx
@@ -8,6 +8,15 @@ function Buttons(props) {
       <button onClick={props.onClick}>Japanese</button>
       <button onClick={props.onClick}>Korean</button>
       <button onClick={props.onClick}>Vietnamese</button>
+      <div>
+        <button onClick={props.onClick}>American</button>
+        <button onClick={props.onClick}>Burmese</button>
+        <button onClick={props.onClick}>Greek</button>
+        <button onClick={props.onClick}>Mexican</button>
+        <button onClick={props.onClick}>Ramen</button>
+        <button onClick={props.onClick}>Sushi</button>
+        <button onClick={props.onClick}>Thai</button>
+      </div>
     </div>
   );
 }
