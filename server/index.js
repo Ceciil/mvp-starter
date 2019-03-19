7aa53d73010547598e517540007634b07b72fa1a
Angela Lee <rubii.kun@gmail.com>
2019-03-19 11:51:03 -0700

    Add post route

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const { getRestaurants, getOneRestaurant } = require("./handler.js");
-const items = require('../database-mongo');
+const { getRestaurants, getOneRestaurant, saveOneRestaurant } = require('./handler.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + "/../react-client/dist"));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get("/api/:search", getRestaurants);
+app.get('/api/:search', getRestaurants);
 
-app.get("/api/category/:category", getOneRestaurant);
+app.get('/api/category/:category', getOneRestaurant);
+
+app.post('/api/restaurant', saveOneRestaurant);
 
 app.listen(3000, function () {
-  console.log("Listening on port 3000!");
+  console.log('Listening on port 3000!');
 });
